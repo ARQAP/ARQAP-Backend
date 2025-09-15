e525c8b4204e3cb9bb6cfb751e26c0ba725d5f09
mateopolci <mateopolci@hotmail.com>
2025-09-15 02:08:05 -0300

    fix(api): fix typos in model, added country preload in GET service

--- src/models/archaeologicalsiteModels.go
+++ src/models/archaeologicalsiteModels.go
@@ -3,8 +3,8 @@ package models
 type ArchaeologicalSiteModel struct {
 	Id          int         `json:"id" gorm:"primaryKey;autoIncrement"`
 	Name        string      `json:"Name" gorm:"column:Name;type:varchar(50);not null"`
-	Ubication   string      `json:"Ubication" gorm:"column:Ubication;type:varchar(50);not null"`
+	Location    string      `json:"Location" gorm:"column:Location;type:varchar(50);not null"`
 	Description string      `json:"Description" gorm:"column:Description;type:varchar(255);not null"`
 	RegionID    int         `json:"regionId" gorm:"column:region_id;not null"`
-	Region      RegionModel `json:"region" gorm:"foreignKey:RegionID;references:Id"`
+	Region      RegionModel `json:"region" gorm:"foreignKey:RegionID;references:ID"`
 }
